b3512aefbb21cb5cafb4ec54fdeba7c2804cb658
Hudoits Dzianis <gudoyt99@gmail.com>
2020-03-24 10:12:06 +0300

    deleted sucs files

--- main.py
+++ main.py
@@ -1,13 +1,57 @@
+from abc import ABC, abstractmethod
 import sys
 
 
-def foo(a,b):
-	return a - b
+class IObserver(ABC):
 
-def bar (a,b):
-	return a **b
+	@abstractmethod
+	def update(data):
+		pass
 
+
+
+class IPublisher(ABC):
+
+
+	@abstractmethod
+	def addSub(self, sub):
+		pass
+
+	@abstractmethod
+	def removeSub(self, sub):
+		pass
+
+	@abstractmethod
+	def notifyAllSubs(self, update_data=""):
+		pass
+
+
+class Forbec(IPublisher):
+
+	def __init__(self, subs=[]):
+		self._subs = subs
+		self.notifyAllSubs("Forbec: we opening!")
+
+	def addSub(self, sub):
+		self._subs.append(sub)
+
+	def removeSub(self, sub):
+		self._subs.remove(sub)
+
+	def notifyAllSubs(self, update_data=""):
+		for sub in self._subs:
+			sub.update(update_data)
+
+
+class Observer(IObserver):
+
+	def __init__(self,id):
+		self._id = id
+
+	def update(self, data):
+		print(data + " [Observer " + str(self._id) + "]")
 
 if __name__ == "__main__":
-	argv = sys.argv
-	print(argv)
+	obs = [Observer(o) for o in range(10)]
+	f = Forbec(obs)
+
